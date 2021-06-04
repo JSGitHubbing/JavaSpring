46a7412809e923b05112d66fbe1e1a1a3e845512
Senpenbanka <jesus.ortega.miguel@gmail.com>
2021-06-04 22:36:05 +0200

    Recipe service implemented Endpoint available to get all the Recipes from the mock repository

---
+++ api-rest/src/main/java/my/portfolio/apirest/model/Recipe.java
@@ -0,0 +1,42 @@
+package my.portfolio.apirest.model;
+
+import java.util.List;
+
+public class Recipe {
+    int id;
+    String name;
+    List<Ingredient> ingredients;
+    List<PreparationStep> steps;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<PreparationStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<PreparationStep> steps) {
+        this.steps = steps;
+    }
+}
