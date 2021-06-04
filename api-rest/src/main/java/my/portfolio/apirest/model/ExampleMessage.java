38fa59215aaf34e87ce3dd54d90db145b173bbf3
Senpenbanka <jesus.ortega.miguel@gmail.com>
2021-06-04 22:13:14 +0200

    Added example message endpoints and test file

---
+++ api-rest/src/main/java/my/portfolio/apirest/model/ExampleMessage.java
@@ -0,0 +1,21 @@
+package my.portfolio.apirest.model;
+
+public class ExampleMessage {
+
+    public String content;
+
+    public ExampleMessage() {
+    }
+
+    public ExampleMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
