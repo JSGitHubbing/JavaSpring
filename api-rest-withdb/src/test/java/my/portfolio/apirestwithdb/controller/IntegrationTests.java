47f371f6d61d2e1003d9e9b53511338e8cdeee77
Senpenbanka <jesus.ortega.miguel@gmail.com>
2021-06-06 12:32:07 +0200

    Added simple Integration Test

---
+++ api-rest-withdb/src/test/java/my/portfolio/apirestwithdb/controller/IntegrationTests.java
@@ -0,0 +1,30 @@
+package my.portfolio.apirestwithdb.controller;
+
+import my.portfolio.apirestwithdb.model.Recipe;
+import my.portfolio.apirestwithdb.util.RecipeDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationTests {
+
+    @Autowired
+    RecipeController recipeController;
+
+    @Test
+    public void testCreateReadDelete() {
+        Recipe recipe = RecipeDTO.getRecipe();
+        Recipe recipeResult = recipeController.create(recipe);
+
+        Iterable<Recipe> recipes = recipeController.read();
+        Assertions.assertThat(recipes).first().hasFieldOrPropertyWithValue("name", recipe.getName());
+
+        recipeController.delete(recipeResult.getId());
+        Assertions.assertThat(recipeController.read()).isEmpty();
+    }
+}
